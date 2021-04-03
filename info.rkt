c45bfb25492a5fbedad50c7ad530d82bbbb43e3c
Suzanne Soy <ligo@suzanne.soy>
2021-04-04 00:09:55 +0100

    Added missing dependencies

--- info.rkt
+++ info.rkt
@@ -5,7 +5,9 @@
                "phc-toolkit"))
 (define build-deps '("base"
                      "reprovide-lang-lib"
-                     "polysemy"))
+                     "polysemy"
+                     "scribble-lib"
+                     "hyper-literate"))
 (define scribblings '(("scribblings/envlang.scrbl" (multi-page))))
 (define pkg-desc "A language with first-class-environments")
 (define version "0.1")
